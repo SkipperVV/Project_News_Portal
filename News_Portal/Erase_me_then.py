2437f3fbb47ec7c7335f07243b0229e125472ec9
SkipperVV <Skippervasin@gmail.com>
2023-09-06 19:23:19 +0200

    06/09/23

---
+++ News_Portal/Erase_me_then.py
@@ -0,0 +1,7 @@
+
+article = 'AR'
+news = 'NW'
+POSITION = [(article, 'Статья'), (news, 'Новость'), ]
+
+post_type = (POSITION['AR'])
+print(post_type)
--- News_Portal/Models/models.py
+++ News_Portal/Models/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Sum, Max
+from django.db.models.functions import Coalesce  # Меняет None to 'пользовательское значение'
+
+article = 'AR'
+news = 'NW'
+POSITION = [(article, 'Статья'), (news, 'Новость'), ]
 
 
 class Author(models.Model):
@@ -7,29 +13,37 @@ class Author(models.Model):
     rating = models.IntegerField(default=0)
 
     def update_rating(self):
-        posts_rating = 0
-        comments_rating = 0
-        post_comments_rating = 0
+        posts_rating = self.post_set.aggregate(posts_rat=Coalesce(Sum('rating'), 0)).get(
+            'posts_rat')  # или через get или получить значение по ключу ['posts_rat']
+        comments_rating = self.user.comment_set.aggregate(posts_rat=Coalesce(Sum('rating'), 0))[
+            'posts_rat']
+        post_comments_rating = self.post_set.aggregate(post_comments_rat=Coalesce(Sum('comment__rating'), 0))[
+            'post_comments_rat']
+        #           или так:
+        # posts_rating = Post.objects.filter(author=self).aggregate(posts_rat=Coalesce(Sum('rating'), 0))[
+        #     'posts_rat']  # ['posts_rat'] обращение по ключу
+        # comments_rating = Comment.objects.filter(user=self.user).aggregate(comments_rat=Coalesce(Sum('rating'), 0))[
+        #     'comments_rat']
+        # post_comments_rating = \
+        # Comment.objects.filter(post__author=self).aggregate(post_comments_rat=Coalesce(Sum('rating'), 0))[
+        #     'post_comments_rat']
 
-        posts = Post.objects.filter(author=self)
-        for i in posts:
-            posts_rating += i.rating
-        comments = Comment.objects.filter(user=self.user)
-        for j in comments:
-            comments_rating += j.rating
-        post_comments = Comment.objects.filter(post__author=self)
-        for k in post_comments:
-            post_comments_rating += k.rating
-
-        print('posts_rating= ', posts_rating)
-        print('comments_rating=', comments_rating)
-        print('post_comments_rating=', post_comments_rating)
+        print('Raiting of author: ', self.user)
+        print('posts rating= ', posts_rating)
+        print('comments rating=', comments_rating)
+        print('post comments rating=', post_comments_rating)
 
         self.rating = posts_rating * 3 + comments_rating + post_comments_rating
         self.save()
 
-    def __str__(self):
-        return (f"Author's name is: %s" % self.user)
+    @staticmethod
+    def get_best_author():  # Вывести username и рейтинг лучшего пользователя
+        max_rating = Author.objects.aggregate(Max("rating"))["rating__max"]
+        best_author = Author.objects.get(rating=max_rating).user
+        print(f'Наибольший рейтинг {max_rating} имеет автор {best_author}')
+
+    # def __str__(self):
+    # return ("автор: %s" % self.user)
 
 
 class Category(models.Model):
@@ -37,18 +51,28 @@ class Category(models.Model):
 
 
 class Post(models.Model):
-    article = 'AR'
-    news = 'NW'
-
-    POSITION = [(article, 'Статья'), (news, 'Новость'), ]
-
     post_time = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=101)
     text = models.TextField()
-    post_type = models.CharField(max_length=10, choices=POSITION, default=news)
+    post_type = models.CharField(max_length=2, choices=POSITION, default=article)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ManyToManyField(Category, through='PostCategory')
+
+    @staticmethod
+    def get_best_post():  # Вывести дату добавления, username автора, рейтинг, заголовок и превью лучшей статьи
+        max_post_rating = Post.objects.aggregate(Max("rating"))["rating__max"]
+        post = Post.objects.get(rating=max_post_rating)
+        post_time = post.post_time.strftime('%d.%m.%Y')
+        post_type = post.post_type
+        author = post.author.user
+        title = post.title
+
+        print(f"Лучшей статьёй, по мнению наших многочисленных подписчиков, является {post_type} '{title}'.\n"
+              f"{post_type} была опубликована {post_time} и уже получила {max_post_rating} миллионов положительных отзывов."
+              f"\nАвтор {post_type} - товарищ {author}\n"
+              f"Краткое изложение {post_type}:")
+        post.preview()
 
     def like(self):
         self.rating += 1
@@ -62,10 +86,6 @@ class Post(models.Model):
         small_text = self.text[0:124] + '...'
         return small_text
 
-class PostCategory(models.Model):  # OneToMany
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
 
 class Comment(models.Model):
     text = models.TextField()
@@ -74,4 +94,15 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def like(self):
+        self.rating += 1
+        self.save()
 
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):  # OneToMany
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
