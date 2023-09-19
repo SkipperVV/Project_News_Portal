63094fa83292d087e157c50804c156d0f15f7547
SkipperVV <Skippervasin@gmail.com>
2023-09-19 16:40:43 +0200

    19/09/23. 1

--- News_Portal/Models/templatetags/filter.py
+++ News_Portal/Models/templatetags/filter.py
@@ -1,27 +1,25 @@
 from django import template
-from django_filters import FilterSet
+from django import forms
+from django_filters import FilterSet, DateFilter
 
 register = template.Library()
 from ..models import Post
 
-
+# class PostFilter(FilterSet):
+#     class Meta:
+#         model = Post
+#         fields = {
+#             'author': ['exact'],
+#             'title': ['icontains'],
+#             'post_time': ['gt'],
+#         }
+# Выбор даты из календаря
 class PostFilter(FilterSet):
+    post_time = DateFilter(field_name='post_time', widget=forms.DateInput(attrs={'type': 'date'}),
+                           lookup_expr='date__gt')
     class Meta:
         model = Post
         fields = {
             'author': ['exact'],
             'title': ['icontains'],
-            'post_time': ['gt'],
         }
-        #Выбор даты из календаря
-# class PostFilter(FilterSet):
-#     post_time = DateFilter(field_name='post_time', widget=forms.DateInput(attrs={'type': 'date'}),
-#                            lookup_expr='date__gt')
-#
-#     class Meta:
-#         model = Post
-#         fields = {
-#             'author': ['exact'],
-#             'title': ['exact'],
-#         }
-
--- News_Portal/Models/views.py
+++ News_Portal/Models/views.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from typing import Any
-from django.shortcuts import render
+
 from django.views.generic import ListView
-from django.core.paginator import Paginator
 
 from .models import Post
 from .templatetags.filter import PostFilter
+
 
 class PostsListAll(ListView):
     model = Post
@@ -13,11 +13,16 @@ class PostsListAll(ListView):
     template_name = 'posts.html'
     context_object_name = 'posts'
 
+    # def sort_choice(self, item_to_sort):
+    #     self.ordering = str(item_to_sort)
+    #     return self.ordering
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
         context['posts_quantity'] = len(Post.objects.all())
         return context
+
 
 class PostView(ListView):
     model = Post
