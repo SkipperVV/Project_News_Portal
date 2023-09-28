0d4a1467645fb29b9993e4dba9e34608404aeb68
SkipperVV <Skippervasin@gmail.com>
2023-09-28 11:20:22 +0200

    28/09/23 -1

--- News_Portal/Models/urls.py
+++ News_Portal/Models/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from .views import PostView, PostsListAll, PostCreateView, PostDeleteView,PostUpdateView
 from .views_old import PostsList, PostDetail
 
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
 urlpatterns = [
     path('', PostsListAll.as_view()),
     path('one_by_one/', PostsList.as_view()),
@@ -12,4 +16,8 @@ urlpatterns = [
     path('create/<int:pk>', PostUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', PostDeleteView.as_view(), name='delete'),
 
+    path('admin/', admin.site.urls),
+    path('', include('protect.urls')),
+    path('sign/', include('sign.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
--- News_Portal/News_Portal/urls.py
+++ News_Portal/News_Portal/urls.py
@@ -1,26 +1,11 @@
-"""
-URL configuration for News_Portal project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
     # path('pages/', include('django.contrib.flatpages.urls')),
-    path('admin/', admin.site.urls),
-    path('login/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    # path('login/', admin.site.urls),
     path('news/', include('Models.urls')),
     path('', include('Models.urls')),
 
@@ -28,5 +13,8 @@ urlpatterns = [
     # path('create/admin/login/',admin.site.urls),
     # path('accounts/login/',admin.site.urls),
     path('accounts/', include('allauth.urls')),
+
+    path('', include('protect.urls')),
+    path('sign/', include('sign.urls')),
 
 ]
