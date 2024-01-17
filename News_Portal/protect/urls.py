e59c9642537d7472ba24257e85ff3cf0de98e5ae
SkipperVV <Skippervasin@gmail.com>
2024-01-17 16:12:00 +0100

    17/01/24
    swagger-ui works, but not adapted

--- News_Portal/protect/urls.py
+++ News_Portal/protect/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import IndexView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
